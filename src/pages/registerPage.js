f11d65209708844872fb6e4f85162baba3a648ed
Jack Delahunt <20085409@mail.wit.ie>
2020-12-03 20:35:59 +0000

    fixed routing issue with register

--- src/pages/registerPage.js
+++ src/pages/registerPage.js
@@ -1,9 +1,9 @@
 import React, { useContext, useEffect, useState } from "react";
-import { Redirect, Router } from "react-router-dom";
+import { Redirect, Router, withRouter } from "react-router-dom";
 import {AuthContext} from "../contexts/authContext";
 import useForm from "react-hook-form";
 
-const RegisterPage = (props) => {
+export default function(props) {
     const context = useContext(AuthContext);
     const { register, handleSubmit, } = useForm();
     const {from} = props.location.state || {from: {pathname: "/"}};
@@ -12,18 +12,10 @@ const RegisterPage = (props) => {
         context.authenticate(data);
     }
 
-    useEffect(() => {
-        console.log("The eagle has landed")
-        if(context.isAuthenticated) {
-            console.log("light this candle")
-            goBack();
-        }
-    }, [context.isAuthenticated]);
-
-    const goBack = () => {
-        console.log("i hear the train a comin")
-        return <Redirect to={from} />;
+    if(context.isAuthenticated) {
+        return <Redirect to={from.pathname} />;
     }
+
 
     return (
         <form className="form bg-dark text-light" onSubmit={handleSubmit(onSubmit)}>
@@ -47,10 +39,8 @@ const RegisterPage = (props) => {
                 />
             </div>    
             <button type="submit" className="btn btn-primary">
-            Submit
+                Submit
             </button>
         </form>
     );
 };
-
-export default RegisterPage;
